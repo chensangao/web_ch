daa0cb7b82b872c745cc821edca6c4cf11b4d9d6
chenhao <985399547@qq.com>
2020-05-19 15:24:15 +0800

    first commit

---
+++ layuiadmin/json/user/session.js
@@ -0,0 +1,9 @@
+{
+  "code": 0
+  ,"msg": ""
+  ,"data": {
+    "username": "陈浩"
+    ,"sex": "男"
+    ,"role": 1
+  }
+}
---
+++ layuiadmin/layui/lay/modules/mobile/layim-mobile-open.js
@@ -0,0 +1,11 @@
+/**
+
+ @Name：layim mobile 开源包
+ @Author：陈浩
+ @License：MIT
+    
+ */
+ 
+layui.define(function(exports){
+  exports('layim-mobile', layui.v);
+});
